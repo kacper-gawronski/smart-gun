bfd6b7609fe74b55d5462068f79941221e1d4858
Kacper Gawroński <kacpergawronski25@gmail.com>
2020-06-04 01:20:33 +0200

    navigation control created
    Wstępne utworzenie kontroli nawigacja-smartwatch oraz funkcje przekazujące dane otrzymane przez SW od MC do nawigacji.

--- src/com/agh/studio/backend/navigation/Navigation.java
+++ src/com/agh/studio/backend/navigation/Navigation.java
@@ -1,4 +1,88 @@
 package com.agh.studio.backend.navigation;
 
+import java.util.Date;
+
 public class Navigation {
+
+    Location currentLocation;
+    Location destinationLocation;
+
+    // duration time in seconds from current location to destination location
+    Integer duration;
+    String durationText;
+
+    // distance in meters from current location to destination location
+    Integer distance;
+    String distanceText;
+
+    // możliwe że to pole nie będzie tutaj potrzebne (po analizie w razie potrzeby - usunąć)
+    Boolean isGoingToIntervention;
+
+    public Navigation(Location location) {
+        this.isGoingToIntervention = false;
+        this.currentLocation = location;
+        this.destinationLocation = location;
+        this.duration = 0;
+        this.durationText = "";
+        this.distance = 0;
+        this.distanceText = "";
+    }
+
+    // GETTERS and SETTERS
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    public Location getDestinationLocation() {
+        return destinationLocation;
+    }
+
+    public void setDestinationLocation(Location destinationLocation) {
+        this.destinationLocation = destinationLocation;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getDurationText() {
+        return durationText;
+    }
+
+    public void setDurationText(String durationText) {
+        this.durationText = durationText;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public String getDistanceText() {
+        return distanceText;
+    }
+
+    public void setDistanceText(String distanceText) {
+        this.distanceText = distanceText;
+    }
+
+    public Boolean getGoingToIntervention() {
+        return isGoingToIntervention;
+    }
+
+    public void setGoingToIntervention(Boolean goingToIntervention) {
+        isGoingToIntervention = goingToIntervention;
+    }
 }
--- src/com/agh/studio/backend/smartwatch/Smartwatch.java
+++ src/com/agh/studio/backend/smartwatch/Smartwatch.java
@@ -1,4 +1,38 @@
 package com.agh.studio.backend.smartwatch;
 
+import com.agh.studio.backend.navigation.Location;
+import com.agh.studio.backend.navigation.Navigation;
+
 public class Smartwatch {
+
+    // Trzeba zaimplementować pola odpowiedzialne za rodzaj interwencji oraz czy paatrol dojeżdża czy jest na miejscu czy po prostu patroluje
+    // w przypadku wezwania do interwencji od mastera następuje wywołanie setNavigationParameters()
+    // w przypadku anulowania wezwania do interwencji następuje wywołanie cancellationOfIntervention()
+
+    Navigation navigation;
+    Location location;
+
+    void updateLocation() {
+        this.location = navigation.getCurrentLocation();
+    }
+
+    void setNavigationParameters(Location destination, Integer duration, Integer distance, String durationText, String distanceText) {
+        navigation.setGoingToIntervention(true);
+        navigation.setDestinationLocation(destination);
+        navigation.setDuration(duration);
+        navigation.setDistance(distance);
+        navigation.setDurationText(durationText);
+        navigation.setDistanceText(distanceText);
+    }
+
+    void cancellationOfIntervention() {
+        navigation.setGoingToIntervention(false);
+        navigation.setDestinationLocation(navigation.getCurrentLocation());
+        navigation.setDuration(0);
+        navigation.setDistance(0);
+        navigation.setDurationText("");
+        navigation.setDistanceText("");
+    }
+
+
 }
