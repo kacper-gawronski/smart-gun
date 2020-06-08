32a8bfb0dd223964ba00fad90bcc0d545bddc67f
Paweł Perończyk <paweu98@gmail.com>
2020-06-08 13:00:05 +0200

    Added ReportGenerator + HQ methods
    HQ:
    - distanceCalculator
    - method isDay - determines whether is day or not
    
    ReportGenerator (to be finished):
    - convert SmartwatchReport to CSV Format String[]
    - write formatted SW Reports to CSV file (to be finished)

--- src/com/agh/studio/backend/headquarter/HeadQuarter.java
+++ src/com/agh/studio/backend/headquarter/HeadQuarter.java
@@ -1,9 +1,42 @@
 package com.agh.studio.backend.headquarter;
 
 import com.agh.studio.backend.navigation.Location;
+import com.agh.studio.backend.smartwatch.SmartwatchReport;
 
 public class HeadQuarter {
 
-    // wstępnie zakładam że patrole wychodzące z kwatery będą dostawały taką początkową lokalizacje (np. komenda wojewódzka Policji w Krakowie)
-    public static Location headQuarterLocation = new Location(50.067258, 19.979021);
+    // wstępnie zakładam że patrole wychodzące z kwatery będą dostawały taką
+    // początkową lokalizacje (np. komenda wojewódzka Policji w Krakowie)
+    public static final Location headQuarterLocation = new Location(50.067258, 19.979021);
+
+    // basic version - computing distance in straight line
+    public static double calculateDistanceBetween(Location objective, Location support) {
+
+        double objectiveLong = Math.toRadians(objective.getLongitude());
+        double objectiveLat = Math.toRadians(objective.getLatitude());
+
+        double supportLong = Math.toRadians(support.getLongitude());
+        double supportLat = Math.toRadians(support.getLatitude());
+
+        double diffLong = supportLong - objectiveLong;
+        double diffLat = supportLat - objectiveLat;
+
+        double a = Math.pow(Math.sin(diffLat / 2), 2)
+                + Math.cos(objectiveLat) * Math.cos(supportLat)
+                + Math.pow(Math.sin(diffLong / 2), 2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double earthRadius = 6371;
+
+        return (c * earthRadius);
+    }
+
+    public static Boolean isDay(SmartwatchReport smartwatchReport) {
+        int hour = smartwatchReport.getReportTime().getHour();
+
+        return (hour <= 6 || hour >= 22);
+    }
+
+
+
 }
---
+++ src/com/agh/studio/backend/reports/ReportGenerator.java
@@ -0,0 +1,58 @@
+package com.agh.studio.backend.reports;
+
+import com.agh.studio.backend.smartwatch.SmartwatchReport;
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportGenerator {
+
+    private static final String[] headerList = {"reportTime", "smartwatchId", "officerId", "gunId",
+                                                "vehicleId", "isGunFired", "patrolLatitude", "patrolLongitude",
+                                                "patrolStatus"};
+
+    public List<SmartwatchReport> smartwatchReportList = new ArrayList<>();
+
+    public void writeSmartwatchReportsToCsvFile(List<SmartwatchReport> swReports) {
+        String filepath = "../new_file.csv";
+
+        File file = new File(filepath);
+
+        try {
+            FileWriter outputFile = new FileWriter(filepath);
+
+            CSVWriter writer = new CSVWriter(outputFile);
+            writer.writeNext(headerList);
+
+            for (SmartwatchReport swReport : swReports) {
+                String[] report = formatSingleReportToCsv(swReport);
+                writer.writeNext(report);
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String[] formatSingleReportToCsv(SmartwatchReport swReport) {
+
+        return new String[]{ String.valueOf(swReport.getReportTime()),
+                            String.valueOf(swReport.getSmartwatchId()),
+                            String.valueOf(swReport.getOfficerId()),
+                            String.valueOf(swReport.getGunId()),
+                            String.valueOf(swReport.getVehicleId()),
+                            String.valueOf(swReport.getGunFired()),
+                            String.valueOf(swReport.getLocation().getLatitude()),
+                            String.valueOf(swReport.getLocation().getLongitude())
+                            //String.valueOf(swReport.getPatrolStatus()) }
+        };
+
+    }
+
+}
